b8058e878c5a8001954d63035a10871e510648f1
Alex <alexhaigh23@outlook.com>
2021-05-25 21:46:19 +0100

    setup fetch function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert('hello!');
+
+const API_URL = 'https://pokeapi.co/api/v2/pokemon/';
+
+
+async function getPokemon() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    console.log(data);
+}
+
+getPokemon();
+
