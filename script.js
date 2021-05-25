6fe6722eb782af2b77bc29a9e27a8707b7b91bfb
Alex <alexhaigh23@outlook.com>
2021-05-26 00:42:25 +0100

    finished fetchh function and started building html templatefor cards

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 
-const API_URL = 'https://pokeapi.co/api/v2/pokemon/';
+const cardsContainer = document.querySelector('.cards');
+const numberOfPokemons = 150;
 
+function getAllPokemon() {
+    for (let i = 1; i <= 4; i++) {
+         getPokemon(i);
+    }
+}
 
-async function getPokemon() {
+async function getPokemon(id) {
+    const API_URL = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const response = await fetch(API_URL);
     const data = await response.json();
     console.log(data);
 }
 
-getPokemon();
+getAllPokemon();
+
 
